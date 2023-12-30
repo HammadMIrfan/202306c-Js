4fd083ab27ee317b54911120a7b2e2cac1eab792
Hammad_Irfan <150622440+HammadMIrfan@users.noreply.github.com>
2023-12-30 13:07:25 +0500

    uploaded 30 dec 23

---
+++ main.js
@@ -0,0 +1,127 @@
+// Types of Javascript
+
+// Internal Or External
+
+// variables making steps
+
+// keyword
+// name
+// operator
+// value
+// terminator
+
+// 3 types of variables
+
+//  var , let , const
+
+// 1 . var example
+
+// console.log(a)
+// var a = 6
+
+// answer is undefined
+
+// allows Redeclaration & Reintializtion
+
+
+// var variable = 1;
+// var variable = 2;
+// var variable = 3;
+// var variable = 4;
+// var variable = 5;
+// var variable = 6;
+// var variable = 7;
+// var variable = 1;
+
+// console.log(variable)
+// console.log(variable)
+
+// var a = 5
+// var b = a = 9
+
+// console.log(a)
+
+// 2 . let
+
+// let a = 5
+// let b = 10
+
+// allow to reintialized values of variables
+
+// let a = 5
+// let b = a = 90
+
+// console.log(a)
+
+// 3 . const
+
+// const a = 5
+// const b = a = 1
+
+// console.log(a)
+
+// console.log(a) 
+// const a = 5
+// const b = a = 1
+//   answer : error facing
+
+
+// form handling
+
+// var a = prompt("enter your name");
+// document.write(a)
+
+
+
+// var a = parseInt(prompt("enter your value 1")) 
+// var b = parseInt(prompt("enter your value 2")) 
+// document.write(a * b)
+
+// var c = prompt("enter your value 1")
+// var d = prompt("enter your value 2")
+// var e = c + d
+// document.write(e)
+
+// fucntions
+// 2 types of fucntions
+
+// Non - parameterized function
+// parameterized function
+
+
+// function abc (){
+//     alert("The function has been called")
+// }
+
+// abc();
+
+
+
+// onclick event
+
+
+// function abc() {
+//   document.write("The function has been called");
+// }
+
+// function xyz(v1 , v2) {
+//   document.write(v1 + v2);
+// }
+
+// xyz(25,95)
+// xyz(65,95)
+// xyz(215,905) 
+
+// if else
+
+// var email = prompt("enter your email")
+// var password = prompt("enter your password")
+
+// if (email == "hammad@gmail.com" && password == 123) {
+//     document.write("welcome to our website")
+// } else {
+//     document.write("invalid credentials")
+    
+// }
+
+
